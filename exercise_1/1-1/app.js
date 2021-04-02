aabeee59ce9bbff9667068d3bc63720c52d53a3b
Jfornqz <61070065@kmitl.ac.th>
2021-04-02 23:51:43 +0700

    edit folder

--- exercise_1/1-1/app.js
+++
@@ -1,8 +0,0 @@
-const checkName = (firstName, lastName) => {
-    if (firstName === 'Shippop') {
-        console.log('Best Online Shipping Platform');
-    } else {
-        console.log(`Hello Shippop, My name is ${firstName} ${lastName}`);
-    };
-}
-checkName('Thotsawat', 'Jantapitak')
